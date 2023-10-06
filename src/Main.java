141ce34e909d9c7470c998959cbbf3e93a4637df
Alejandra.D <alejandra.davila.tipan@udla.edu.ec>
2023-10-06 08:55:31 -0500

    Constructores, encapsulacion y entrada de datos por consola

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -7,8 +9,8 @@ public class Main {
         auto1.setColor("Blanco");
         auto1.imprimirColorPuertas(1);
 
-        Duenio duenio1 = new Duenio("Pablo", 1725490674, 28, 'M');
-        duenio1.imprimirDuenio();
+        Duenio duenio1 = new Duenio("Pablo", 208059464, 28, 'M');
+        //duenio1.imprimirDuenio();
 
         Motor motor1 = new Motor(2.0, "Diesel");
         motor1.imprimirMotor();
@@ -20,13 +22,40 @@ public class Main {
 
         System.out.println("------------------------------------------------------");
 
-        Duenio duenio2= new Duenio("Juan", 171551028, 34, 'M');
+        Duenio duenio2= new Duenio("Juan", 1758496240, 34, 'M');
         Motor motor2 = new Motor(4.0, "Extra");
 
         Auto auto2 = new Auto(4, "Rojo", 3, motor2, duenio2);
         auto2.imprimirColorPuertas(2);
         auto2.getDuenio().imprimirDuenio();
         auto2.getMotor().imprimirMotor();
+
+        System.out.println("------------------------------------------------------");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el numero de ruedas: ");
+        int numRuedas = sc.nextInt();
+        System.out.println("Ingrese el color del vehiculo: ");
+        String color = sc.next();
+        System.out.println("Ingrese el numero de puertas: ");
+        int numPuertas = sc.nextInt();
+        System.out.println("Ingrese el cilindraje del motor: ");
+        double cilindraje = sc.nextDouble();
+        System.out.println("Ingrese el tipo de combustible: ");
+        String combustible = sc.next();
+        Motor motor3 = new Motor(cilindraje,combustible);
+        System.out.println("Ingrese el nombre del dueño: ");
+        String nombre = sc.next();
+        System.out.println("Ingrese edad del dueño");
+        int edad = sc.nextInt();
+        System.out.println("Ingrese la cedula del dueño: ");
+        int cedula = sc.nextInt();
+        System.out.println("Ingrese el genero del dueño: ");
+        char genero = sc.next().charAt(0);
+        Duenio duenio3 = new Duenio(nombre,cedula,edad,genero);
+        Auto auto3 = new Auto(numRuedas, color, numPuertas, motor3, duenio3);
+        auto3.imprimirColorPuertas(3);
+        auto3.getMotor().imprimirMotor();
+        auto3.getDuenio().imprimirDuenio();
 
     }
 }
